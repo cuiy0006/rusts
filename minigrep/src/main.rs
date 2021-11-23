88396c628003d34337375562a7b13a3c02bee478
Yao Cui <cuiy0006@gmail.com>
2021-11-22 19:46:55 -0500

    import

--- minigrep/src/main.rs
+++ minigrep/src/main.rs
@@ -1,6 +1,5 @@
 use std::{ process, env };
-mod lib;
-use lib::Config;
+use minigrep::{ Config, run };
 
 fn main() {
     let config = Config::new(env::args()).unwrap_or_else(|err| {
@@ -8,7 +7,7 @@ fn main() {
         process::exit(1);
     });
 
-    if let Err(err) = lib::run(&config) {
+    if let Err(err) = run(&config) {
         eprintln!("Application error: {}", err);
         process::exit(1);
     }
